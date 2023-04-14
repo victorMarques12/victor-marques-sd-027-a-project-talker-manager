298c6e9b17607d460669f96e515fda1c1c98dc79
victor marques <victormarques.ym@gmail.com>
2023-04-14 15:46:09 -0300

    req1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -7,10 +9,12 @@ const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+app.get('/talker', async (_request, response) => {
+  const contentPath = path.resolve(__dirname, 'talker.json');
+  const data = await fs.readFile(contentPath, 'utf-8');
+  const talkersData = JSON.parse(data);
+  return response.status(HTTP_OK_STATUS).json(talkersData);
 });
-
 
 app.listen(PORT, () => {
   console.log('Online');
